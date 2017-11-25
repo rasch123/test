1b610b41fe3bdf6eab42ea8ca58f6cba9d9af2eb
rasch123 <31843150+rasch123@users.noreply.github.com>
2017-11-25 12:29:49 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -182,7 +182,7 @@ var fs = require('fs');
 var voiceChannelID = "356496696282447883";
 var izy = "66186356581208064";
 const ytdl = require('ytdl-core');
-var stream = ytdl('https://www.youtube.com/watch?v=GXTn4okvZ94', {filter : 'audioonly'});
+
 bot.on("message", function (user, userID, channelID, message, rawEvent)
 {
     //http://www.w3schools.com/jsref/jsref_substring.asp
@@ -205,8 +205,8 @@ bot.joinVoiceChannel(voiceChannelID, function(error, events) {
     //Create a stream to your file and pipe it to the stream
     //Without {end: false}, it would close up the stream, so make sure to include that.
  
+bot.ytdl('https://www.youtube.com/watch?v=oTdr1iSqge8', { filter : 'audioonly' }); 
 
-bot.voiceConnection.playStream(stream);
     //The stream fires `done` when it's got nothing else to send to Discord.
     stream.on('done', function() {
        //Handle

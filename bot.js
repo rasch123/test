fc53f4932dbbcd4557f3bf64f69e7ae12831a46f
rasch123 <31843150+rasch123@users.noreply.github.com>
2017-11-26 15:00:36 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -182,11 +182,7 @@ var fs = require('fs');
 var voiceChannelID = "356650775071424520";
 var izy = "66186356581208064";
 const ytdl = require('ytdl-core');
-const yama = require('yama-stream');
-const url = 'https://www.youtube.com/watch?v=2LmWBaFj160';
-const decoder = require('lame').Decoder;
-const speaker = require('speaker');
-
+var stream = ytdl('https://www.youtube.com/watch?v=GXTn4okvZ94', {filter : 'audioonly'});
 bot.on("message", function (user, userID, channelID, message, rawEvent)
 {
     //http://www.w3schools.com/jsref/jsref_substring.asp
@@ -210,9 +206,7 @@ bot.joinVoiceChannel(voiceChannelID, function(error, events) {
     //Without {end: false}, it would close up the stream, so make sure to include that.
  
 
-yama(url, "0:10",10, 10)
-    .pipe(decoder())
-    .pipe(speaker())
+let dispatcher = voiceConnection.playStream(stream);
     //The stream fires `done` when it's got nothing else to send to Discord.
     stream.on('done', function() {
        //Handle
